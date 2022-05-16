ff2cd2d9b635906ac61e6a8021780dad68281d90
Klemens Galus <galus.klemens@gmail.com>
2022-05-16 12:08:41 +0200

    ,
    ,

--- src/app/end/end.component.ts
+++ src/app/end/end.component.ts
@@ -10,6 +10,13 @@ export interface Game {
   pseudo: string;
   id: number;
 }
+export interface Clicks {
+  time: number;
+  gameId: number;
+  clickNumber: number;
+  id: number;
+}
+
 
 @Component({
   selector: 'app-end',
@@ -20,16 +27,24 @@ export interface Game {
 
 export class EndComponent implements OnInit {
   scores$!: Observable<Game[]> //score de l'utilisateur
-  lastGameClicks$!: Observable<any[]>
+  lastGameClicks$!: Observable<Clicks[]>
   lastGame$!: Observable<any>
+  
   id!:number
   constructor(private service: GameApiService, private route: ActivatedRoute) {
+    
+  }
+  ngOnInit(): void {
     this.scores$ = this.service.getGamesRanksByName(sessionStorage.getItem('name'))
     var stringId: string | null = this.route.snapshot.paramMap.get('id')
     this.id = parseInt(stringId!);
-    this.lastGameClicks$ = this.service.getCurrentGamesSorted(this.id)
     this.lastGame$ = this.service.getGamesRanksById(this.id)
+    setTimeout(() => { //par moment les clics ne s'affichent pas tous ici on va les recharger juste après le chargement de la page
+      this.lastGameClicks$ = this.service.getCurrentGamesSorted(this.id)
+    }, 100)
   }
-  ngOnInit(): void {}
-
+  clicScoreList$!: Observable<any>
+  showClicks(id: number) { //affichage des clics de la partie selectionnée
+    this.clicScoreList$ = this.service.getCurrentGamesSorted(id)
+  }
 }
